72b4f06e8bcc4ff9182a96474d24c3391310addf
jaroncui <jaron.cui@gmail.com>
2024-02-14 20:01:30 -0500

    done with a3

--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -4,6 +4,7 @@ import { HiMiniBars3 } from "react-icons/hi2";
 import CourseNavigation from "./Navigation";
 import Modules from "./Modules";
 import Home from "./Home";
+import Assignments from "./Assignments";
 function Courses() {
   const { courseId } = useParams();
   const course = courses.find((course) => course._id === courseId);
@@ -21,7 +22,7 @@ function Courses() {
               <Route path="Home" element={<Home/>} />
               <Route path="Modules" element={<Modules/>} />
               <Route path="Piazza" element={<h1>Piazza</h1>} />
-              <Route path="Assignments" element={<h1>Assignments</h1>} />
+              <Route path="Assignments" element={<Assignments/>} />
               <Route path="Assignments/:assignmentId" element={<h1>Assignment Editor</h1>} />
               <Route path="Grades" element={<h1>Grades</h1>} />
             </Routes>

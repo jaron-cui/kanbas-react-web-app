a36c2d96409960379aac5f59f6b142e2f49a816b
jaroncui <jaron.cui@gmail.com>
2024-02-21 18:58:21 -0500

    lab 4

--- src/Labs/a3/index.tsx
+++ src/Labs/a3/index.tsx
@@ -1,3 +1,5 @@
+import { useSelector } from "react-redux";
+import { LabState } from "../store";
 import Add from "./Add";
 import ConditionalOutput from "./ConditionalOutput";
 import Highlight from "./Highlight";
@@ -8,9 +10,17 @@ import Styles from "./css/Styles";
 import PathParameters from "./routing/PathParameters";
 
 function Assignment3() {
+  const { todos } = useSelector((state: LabState) => state.todosReducer);
   return (
     <div className="container">
       <h1>Assignment 3</h1>
+      <ul className="list-group">
+        {todos.map((todo) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
       <ConditionalOutput/>
       <Styles/>
       <Classes/>

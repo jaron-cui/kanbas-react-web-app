b4b17da1a5643e91d344babbd53a1387ef5522ea
jaroncui <jaron.cui@gmail.com>
2024-03-20 19:54:13 -0400

    about to start a5 section 4

--- src/Labs/a5/WorkingWithArrays.tsx
+++ src/Labs/a5/WorkingWithArrays.tsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function WorkingWithArrays() {
   const API = "http://localhost:4000/a5/todos";
@@ -9,6 +10,45 @@ function WorkingWithArrays() {
     due: "2021-09-09",
     completed: false,
   });
+  const [todos, setTodos] = useState<typeof todo[]>([]);
+  const postTodo = async () => {
+    const response = await axios.post(API, todo);
+    setTodos([...todos, response.data]);
+  };
+  const fetchTodos = async () => {
+    const response = await axios.get(API);
+    setTodos(response.data);
+  };
+  const removeTodo = async (removedTodo: typeof todo) => {
+    const response = await axios
+      .get(`${API}/${removedTodo.id}/delete`);
+    setTodos(response.data);
+  };
+  const fetchTodoById = async (id: number) => {
+    const response = await axios.get(`${API}/${id}`);
+    setTodo(response.data);
+  }
+  const createTodo = async () => {
+    const response = await axios.get(`${API}/create`);
+    setTodos(response.data);
+  };
+  const updateTitle = async () => {
+    const response = await axios.get(`${API}/${todo.id}/title/${todo.title}`);
+    setTodos(response.data);
+  };
+  const deleteTodo = async (deleteTodo: typeof todo) => {
+    const response = await axios.delete(`${API}/${deleteTodo.id}`);
+    setTodos(todos.filter((t) => t.id !== deleteTodo.id));
+  };
+  const updateTodo = async () => {
+    const response = await axios.put(`${API}/${todo.id}`, todo);
+    setTodos(todos.map((t) => (t.id === todo.id ? todo : t)));
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
 
   return (
     <div>
@@ -56,6 +96,51 @@ function WorkingWithArrays() {
         onChange={(e) => setTodo({ ...todo,
             completed: !!e.target.value })}
         checked={todo.completed}/>
+      <button onClick={createTodo} >
+        Create Todo
+      </button>
+      <button onClick={updateTitle} >
+        Update Title
+      </button>
+      <textarea value={todo.description}
+        onChange={(e) => setTodo({ ...todo,
+          description: e.target.value })} />
+      <input value={todo.due} type="date"
+        onChange={(e) => setTodo({
+          ...todo, due: e.target.value })} />
+      <label>
+        <input value={'' + todo.completed} type="checkbox"
+          onChange={(e) => setTodo({
+            ...todo, completed: e.target.checked })} />
+        Completed
+      </label>
+      <button onClick={postTodo}> Post Todo </button>
+      <button onClick={updateTodo}>
+        Update Todo
+      </button>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            <input checked={todo.completed}
+              type="checkbox" readOnly />
+            {todo.title}
+            <p>{todo.description}</p>
+            <p>{todo.due}</p>
+            <button onClick={() => fetchTodoById(todo.id)} >
+              Edit
+            </button>
+            <button onClick={() => removeTodo(todo)} >
+              Remove
+            </button>
+            <button onClick={() => deleteTodo(todo)}
+              className="btn btn-danger float-end ms-2">
+              Delete
+            </button>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+      
     </div>
   );
 }
--- src/Labs/a5/WorkingWithObjects.tsx
+++ src/Labs/a5/WorkingWithObjects.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 function WorkingWithObjects() {
   const [assignment, setAssignment] = useState({
     id: 1, title: "NodeJS Assignment",
@@ -12,10 +13,33 @@ function WorkingWithObjects() {
     course: "CS4455"
   });
 
-  const ASSIGNMENT_URL = "http://localhost:4000/a5/assignment"
+  const ASSIGNMENT_URL = "http://localhost:4000/a5/assignment";
+  const fetchAssignment = async () => {
+    const response = await axios.get(`${ASSIGNMENT_URL}`);
+    setAssignment(response.data);
+  };
+  const updateTitle = async () => {
+    const response = await axios
+      .get(`${ASSIGNMENT_URL}/title/${assignment.title}`);
+    setAssignment(response.data);
+  };
+  useEffect(() => {
+    fetchAssignment();
+  }, []);
+
   return (
     <div>
       <h3>Working With Objects</h3>
+      <h3>Modifying Properties</h3>
+      <input onChange={(e) => setAssignment({
+            ...assignment, title: e.target.value })}
+        value={assignment.title} type="text" />
+      <button onClick={updateTitle} >
+        Update Title to: {assignment.title}
+      </button>
+      <button onClick={fetchAssignment} >
+        Fetch Assignment
+      </button>
       <h4>Retrieving Objects</h4>
       <a href="http://localhost:4000/a5/assignment" className="btn btn-primary">
         Get Assignment
